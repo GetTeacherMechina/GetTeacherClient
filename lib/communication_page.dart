7cae320f016b78c042ff8c39de2813a384244782
Ofri Haviv <haviv.ofri@gmail.com>
2024-11-27 16:46:22 +0200

    not working yet, but close

--- lib/communication_page.dart
+++ lib/communication_page.dart
@@ -12,7 +12,17 @@ class _CommunicationPageState extends State<CommunicationPage> {
   late RTCPeerConnection _conn;
   final _localRenderer = RTCVideoRenderer();
   final _remoteRenderer = RTCVideoRenderer();
+  final _offerController = TextEditingController();
+  final _remoteController = TextEditingController();
   MediaStream? _localStream;
+  String message = "";
+
+  @override
+  void initState() {
+    super.initState();
+    _initRenderers();
+    _startWebRTC();
+  }
 
   @override
   void dispose() {
@@ -57,24 +67,64 @@ class _CommunicationPageState extends State<CommunicationPage> {
     // Create offer
     var offer = await _conn.createOffer();
     await _conn.setLocalDescription(offer);
+    _offerController.text = "${offer.sdp ?? ""}|||||${offer.type ?? ""}";
+  }
+
+  Future<void> _loadRemote() async {
+    final data = _remoteController.text.split("|||||");
+    final sdp = data[0];
+    final type = data[1];
+
+    await _conn.setRemoteDescription(RTCSessionDescription(sdp, type));
   }
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Column(
-          children: [
-            const Text("local video"),
-            RTCVideoView(_localRenderer)
-          ],
+        Expanded(
+          child: Column(
+            children: [
+              const Text("local video"),
+              SizedBox(
+                height: 200,
+                width: 200,
+                child: RTCVideoView(
+                  _localRenderer,
+                  mirror: true,
+                  objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                ),
+              ),
+              const Text("remote video"),
+              SizedBox(
+                height: 200,
+                width: 200,
+                child: RTCVideoView(
+                  _remoteRenderer,
+                  objectFit: RTCVideoViewObjectFit.RTCVideoViewObjectFitCover,
+                ),
+              ),
+            ],
+          ),
         ),
-        Column(
-          children: [
-            const Text("remote video"),
-            RTCVideoView(_remoteRenderer)
-          ],
+        SizedBox(
+          height: 100,
+          child: TextField(
+            controller: _offerController,
+            minLines: 10,
+            maxLines: 1000,
+          ),
         ),
+        SizedBox(
+          height: 100,
+          child: TextField(
+            controller: _remoteController,
+            minLines: 10,
+            maxLines: 1000,
+          ),
+        ),
+        ElevatedButton(
+            onPressed: () => _loadRemote(), child: const Text("Load()"))
       ],
     );
   }
