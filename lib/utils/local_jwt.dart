0cd613d822a447807af75aded8f82be7ed7f7dab
Ofri Haviv <haviv.ofri@gmail.com>
2024-12-25 20:09:45 +0200

    fixed formatting

--- lib/utils/local_jwt.dart
+++ lib/utils/local_jwt.dart
@@ -4,7 +4,7 @@ class LocalJwt {
   static Future<String?> getLocalJwt() =>
       SharedPreferencesAsync().getString("local_jwt");
   static void setLocalJwt(final String jwt) =>
-      SharedPreferencesAsync().setString("local_jwt", jwt );
+      SharedPreferencesAsync().setString("local_jwt", jwt);
 
   static void clearJwt() => SharedPreferencesAsync().remove("local_jwt");
 }
