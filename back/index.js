071da57311ecf43b8d3c74b411dddd0d2cb2339e
Ohad-Shaked-Mech <ohadshakedmechina@gmail.com>
2025-01-06 20:15:14 +0200

    fixed webrtc mock server and client

--- back/index.js
+++ back/index.js
@@ -1,54 +1,67 @@
-let port = process.env.PORT || 5000;
+// server.js
+const fs = require('fs');
+const http = require('http');
+const express = require('express');
+const { Server } = require('socket.io');
 
-let IO = require("socket.io")(port, {
+const offerMap = {};
+
+const app = express();
+const httpsServer = http.createServer(app);
+
+const io = new Server(httpsServer, {
   cors: {
-    origin: "*",
-    methods: ["GET", "POST"],
+    origin: '*',
   },
 });
 
-IO.use((socket, next) => {
-  if (socket.handshake.query) {
-    let callerId = socket.handshake.query.callerId;
-    socket.user = callerId;
-    next();
-  }
+app.get('/', (req, res) => {
+  res.send('WebRTC Signaling Server is running.');
 });
 
-IO.on("connection", (socket) => {
-  console.log(socket.user, "Connected");
-  socket.join(socket.user);
+httpsServer.listen(4433, () => {
+  console.log('HTTPS Signaling Server listening on port 4433');
+});
 
-  socket.on("makeCall", (data) => {
-    let calleeId = data.calleeId;
-    let sdpOffer = data.sdpOffer;
-    console.log("making call");
-    socket.to(calleeId).emit("newCall", {
-      callerId: socket.user,
-      sdpOffer: sdpOffer,
+io.on('connection', (socket) => {
+  console.log('A client connected:', socket.id);
+
+  socket.on('join-call', (callId) => {
+    socket.join(callId);
+    console.log(`${socket.id} joined call: ${callId}`);
+    console.log('offer Map 1:', offerMap);
+    if (offerMap[callId]) {
+      socket.emit('offer', {
+        sdp: offerMap[callId].sdp,
+        type: offerMap[callId].type
+      }); 
+    }
+  });
+
+  socket.on('offer', (payload) => {
+    console.log('Got offer from:', socket.id, 'payload:', payload);
+    console.log('offer Map 2:', offerMap);
+    const { callId, sdp, type } = payload;
+    offerMap[callId] = { sdp, type };
+    socket.to(callId).emit('offer', { sdp, type });
+  });
+
+  socket.on('answer', (payload) => {
+    console.log('Got answer from:', socket.id, 'payload:', payload);
+    socket.to(payload.callId).emit('answer', {
+      sdp: payload.sdp,
+      type: payload.type,
     });
   });
 
-  socket.on("answerCall", (data) => {
-    let callerId = data.callerId;
-    let sdpAnswer = data.sdpAnswer;
-
-    console.log("got answer to call");
-    socket.to(callerId).emit("callAnswered", {
-      callee: socket.user,
-      sdpAnswer: sdpAnswer,
+  socket.on('candidate', (payload) => {
+    console.log('Got candidate from:', socket.id, 'payload:', payload);
+    socket.to(payload.callId).emit('candidate', {
+      candidate: payload.candidate,
     });
   });
 
-  socket.on("iceCandidate", (data) => {
-    let calleeId = data.calleeId;
-    let iceCandidate = data.iceCandidate;
-    console.log(data.iceCandidate);
-    console.log(`got ice to ${calleeId}`);  
-
-    console.log(socket.to(calleeId.toString()).emit("iceCandidate", {
-      callee: socket.user,
-      iceCandidate: iceCandidate,
-    }));
+  socket.on('disconnect', () => {
+    console.log('Client disconnected:', socket.id);
   });
 });
--- front/lib/main.dart
+++ front/lib/main.dart
@@ -1,120 +1,397 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
-import 'package:front/views/call_screen/call_screen.dart';
+import 'package:flutter_webrtc/flutter_webrtc.dart';
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+import 'dart:io' show Platform;       // Allows Platform checks
+import 'package:flutter/foundation.dart' show kIsWeb; // Detects web
 
 void main() {
   runApp(const MyApp());
 }
-
+String getSignalingServerUrl() {
+  // for emulator
+  if (kIsWeb) {
+    return 'http://localhost:4433';
+  } else if (Platform.isAndroid) {
+    return 'http://10.0.2.2:4433';
+  } else {
+    return 'http://localhost:4433';
+  }
+}
+String generateCallId([int length = 6]) {
+  const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
+  final rnd = Random.secure();
+  return List.generate(length, (_) => chars[rnd.nextInt(chars.length)]).join();
+}
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      title: 'Flutter WebRTC Demo',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  final String _signalingServerUrl = getSignalingServerUrl();
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  late IO.Socket _socket;
+
+  String _currentCallId = '';
+
+  final TextEditingController _callIdController = TextEditingController();
+
+  RTCPeerConnection? _peerConnection;
+  MediaStream? _localStream;
+
+  final _localRenderer = RTCVideoRenderer();
+  final _remoteRenderer = RTCVideoRenderer();
+
+  bool _isCalling = false;
+
+  bool _isVideoEnabled = true;
+  bool _isAudioEnabled = true;
+
+  final Map<String, dynamic> _iceServers = {
+    'iceServers': [
+      {'urls': 'stun:stun.l.google.com:19302'},
+      {
+      'urls': 'turn:192.158.29.39:3478?transport=udp',
+      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      'username': '28224511:1379330808'
+    },
+    {
+      'urls': 'turn:192.158.29.39:3478?transport=tcp',
+      'credential': 'JZEOEt2V3Qb0y27GRntt2u2PAYA=',
+      'username': '28224511:1379330808'
+    }
+    ],
+  };
+
+  @override
+  void initState() {
+    super.initState();
+    _initRenderers();
+    _connectToSignalingServer();
   }
 
   @override
+  void dispose() {
+    _localRenderer.dispose();
+    _remoteRenderer.dispose();
+    _socket.dispose();
+    super.dispose();
+  }
+
+  Future<void> _initRenderers() async {
+    if (_peerConnection == null) {
+      await _createPeerConnection();
+      debugPrint('PeerConnection created');
+    }
+    await _localRenderer.initialize();
+    await _remoteRenderer.initialize();
+  }
+
+  void _connectToSignalingServer() {
+    _socket = IO.io(
+      _signalingServerUrl,
+      IO.OptionBuilder()
+          .setTransports(['websocket'])
+          .disableAutoConnect()
+          .setExtraHeaders({'Content-Type': 'application/json'})
+          .build(),
+    );
+
+    _socket.connect();
+
+    _socket.onConnect((_) {
+      debugPrint('Connected to signaling server');
+    });
+
+    _socket.on('offer', (data) async {
+      debugPrint('Received offer: $data');
+      final description = RTCSessionDescription(
+        data['sdp'],
+        data['type'],
+      );
+
+      if (_peerConnection == null) {
+        await _createPeerConnection();
+      }
+
+      try {
+        await _peerConnection?.setRemoteDescription(description);
+        debugPrint('setRemoteDescription(offer) succeeded.');
+      } catch (e) {
+        debugPrint('Error setting remote description (offer): $e');
+      }
+
+      final answer = await _peerConnection?.createAnswer();
+      if (answer != null) {
+        await _peerConnection?.setLocalDescription(answer);
+        _socket.emit('answer', {
+          'callId': _currentCallId,
+          'sdp': answer.sdp,
+          'type': answer.type,
+        });
+        debugPrint('Sent answer to server.');
+      }
+    });
+
+    _socket.on('answer', (data) async {
+      debugPrint('Received answer: $data');
+      if (_peerConnection == null) return;
+      final description = RTCSessionDescription(
+        data['sdp'],
+        data['type'],
+      );
+      try {
+        await _peerConnection!.setRemoteDescription(description);
+        debugPrint('Caller set remote description (answer) successfully.');
+      } catch (e) {
+        debugPrint('Error setting remote description (answer): $e');
+      }
+    });
+
+    _socket.on('candidate', (data) async {
+      debugPrint('Received candidate: $data');
+      if (_peerConnection == null) return;
+
+      final candidate = RTCIceCandidate(
+        data['candidate']['candidate'],
+        data['candidate']['sdpMid'],
+        data['candidate']['sdpMLineIndex'],
+      );
+      await _peerConnection?.addCandidate(candidate);
+      debugPrint('addCandidate succeeded.');
+    });
+
+    _socket.onDisconnect((_) {
+      debugPrint('Disconnected from signaling server');
+    });
+  }
+
+  void _joinCallById(String callId) {
+    _currentCallId = callId;
+    _socket.emit('join-call', callId);
+    debugPrint('emitted join call');
+  }
+
+  Future<void> _createPeerConnection() async {
+    _peerConnection = await createPeerConnection(_iceServers);
+
+    _peerConnection?.onIceConnectionState = (RTCIceConnectionState state) {
+      debugPrint('ICE state changed: $state');
+    };
+
+    _peerConnection?.onConnectionState = (RTCPeerConnectionState state) {
+      debugPrint('Connection state changed: $state');
+    };
+
+    if (_localStream == null) {
+      _localStream = await navigator.mediaDevices
+          .getUserMedia({'video': true, 'audio': true});
+      _localRenderer.srcObject = _localStream;
+
+      for (var track in _localStream!.getTracks()) {
+        _peerConnection?.addTrack(track, _localStream!);
+      }
+    }
+
+    _peerConnection?.onTrack = (RTCTrackEvent event) {
+      debugPrint('Got remote track: ${event.track.id}');
+      if (event.streams.isNotEmpty) {
+        setState(() {
+          _remoteRenderer.srcObject = event.streams.first;
+        });
+      }
+    };
+
+    _peerConnection?.onIceCandidate = (RTCIceCandidate candidate) {
+      debugPrint('Sending candidate: ${candidate.toMap()}');
+      _socket.emit('candidate', {
+        'callId': _currentCallId,
+        'candidate': candidate.toMap(),
+      });
+    };
+
+    _peerConnection?.onConnectionState = (state) {
+      debugPrint('Connection state changed: $state');
+    };
+  }
+
+  Future<void> _startCall([String id = "-1"]) async {
+    debugPrint('StartCall button pressed');
+    final newCallId = (id=="-1") ? generateCallId() : id;
+    debugPrint('Generated call ID: $newCallId');
+    _joinCallById(newCallId);
+    debugPrint('Joined call ID: $_currentCallId');
+    if (_peerConnection == null) {
+      await _createPeerConnection();
+      debugPrint('PeerConnection created');
+    }
+    try {
+      final offer = await _peerConnection!.createOffer();
+      debugPrint('Created offer: $offer');
+
+      await _peerConnection!.setLocalDescription(offer);
+      debugPrint('Local description set: $offer');
+
+      _socket.emit('offer', {
+        'callId': _currentCallId,
+        'sdp': offer.sdp,
+        'type': offer.type,
+      });
+      debugPrint('Emitted offer to server');
+  } catch (e, s) {
+    debugPrint('Error in createOffer or setLocalDescription: $e\n$s');
+  }
+    setState(() {
+      _isCalling = true;
+    });
+  }
+
+  Future<void> _joinCall() async {
+  final callId = _callIdController.text.trim();
+  if (callId.isEmpty) {
+    debugPrint('No call ID entered');
+    return;
+  }
+
+  _joinCallById(callId);
+
+  if (_peerConnection == null) {
+    await _createPeerConnection();
+    debugPrint('PeerConnection created in _joinCall().');
+  }
+
+  setState(() {
+    _isCalling = true;
+    _currentCallId = callId;
+  });
+}
+
+  Future<void> _hangUp() async {
+    await _peerConnection?.close();
+    _peerConnection = null;
+
+    _localStream?.dispose();
+    _localStream = null;
+
+    setState(() {
+      _remoteRenderer.srcObject = null;
+      _localRenderer.srcObject = null;
+      _isCalling = false;
+      _currentCallId = '';
+    });
+  }
+
+  void _toggleVideo() {
+  final videoTrack = _localStream?.getVideoTracks().first;
+  if (videoTrack != null) {
+    videoTrack.enabled = !videoTrack.enabled;
+    setState(() {
+      _isVideoEnabled = videoTrack.enabled;
+    });
+  }
+}
+
+void _toggleAudio() {
+  final audioTrack = _localStream?.getAudioTracks().first;
+  if (audioTrack != null) {
+    audioTrack.enabled = !audioTrack.enabled;
+    setState(() {
+      _isAudioEnabled = audioTrack.enabled;
+    });
+  }
+}
+
+  @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-            Padding(
-              padding: const EdgeInsets.all(10),
-              child: ElevatedButton(
-                  onPressed: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (_) => const CallScreen()));
-                  },
-                  child: const Text("Call")),
-            )
-          ],
+        title: Text(
+          _currentCallId.isNotEmpty
+              ? 'Call ID: $_currentCallId'
+              : 'Flutter WebRTC Demo',
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Column(
+        children: [
+          if (!_isCalling) Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              controller: _callIdController,
+              decoration: const InputDecoration(
+                labelText: 'Enter Call ID',
+                hintText: 'E.g. ABC123',
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              ElevatedButton(
+                onPressed: _isCalling ? null : _startCall,
+                child: const Text('Start New Call'),
+              ),
+              const SizedBox(width: 8),
+              ElevatedButton(
+                onPressed: _isCalling ? null : _joinCall,
+                child: const Text('Join Existing Call'),
+              ),
+              const SizedBox(width: 8),
+              ElevatedButton(
+                onPressed: _isCalling ? _hangUp : null,
+                child: const Text('Hang Up'),
+              ),
+            ],
+          ),
+          Expanded(
+            child: Row(
+              children: [
+                if (_isCalling)
+                  Expanded(
+                    child: RTCVideoView(_localRenderer, mirror: true),
+                  ),
+                Expanded(
+                  child: RTCVideoView(_remoteRenderer),
+                ),
+              ],
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              IconButton(
+                icon: Icon(
+                  _isVideoEnabled ? Icons.videocam : Icons.videocam_off,
+                ),
+                onPressed: _toggleVideo,
+              ),
+              IconButton(
+                icon: Icon(
+                  _isAudioEnabled ? Icons.mic : Icons.mic_off,
+                ),
+                onPressed: _toggleAudio,
+              ),
+            ],
+          ),
+          const SizedBox(height: 10),
+        ],
+      ),
     );
   }
 }
