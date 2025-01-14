8022e2a382049c79ecebcf4b246abfeae3f511fc
unknown <mechina-hanich-25@greendreamteam.onmicrosoft.com>
2025-01-14 11:03:08 +0200

    Linting errors and server ip

--- lib/views/call_screen.dart
+++ lib/views/call_screen.dart
@@ -1,28 +1,22 @@
 import "package:flutter/material.dart";
-import "dart:math";
 import "package:flutter_webrtc/flutter_webrtc.dart";
-import "package:socket_io_client/socket_io_client.dart" as IO;
-import "dart:io" show Platform;       // Allows Platform checks
+import "package:getteacher/net/net.dart";
+import "package:socket_io_client/socket_io_client.dart" as io;
+import "dart:io" show Platform; // Allows Platform checks
 import "package:flutter/foundation.dart" show kIsWeb; // Detects web
 
-String getSignalingServerUrl() {
-  // for emulator
-  if (kIsWeb) {
-    return "http://localhost:4433";
-  } else if (Platform.isAndroid) {
-    return "http://10.0.2.2:4433";
-  } else {
-    return "http://localhost:4433";
-  }
-}
-String generateCallId([int length = 6]) {
-  const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  final rnd = Random.secure();
-  return List.generate(length, (_) => chars[rnd.nextInt(chars.length)]).join();
-}
+final String url = kIsWeb
+    ? "http://$host:4433"
+    : Platform.isAndroid
+        ? "http://10.0.2.2:4433"
+        : "http://localhost:4433";
 
 class CallScreen extends StatefulWidget {
-  const CallScreen({super.key,required this.guid,required this.shouldStartCall});
+  const CallScreen({
+    super.key,
+    required this.guid,
+    required this.shouldStartCall,
+  });
   final String guid;
   final bool shouldStartCall;
   @override
@@ -30,38 +24,32 @@ class CallScreen extends StatefulWidget {
 }
 
 class _CallScreenState extends State<CallScreen> {
-  final String _signalingServerUrl = getSignalingServerUrl();
-
-  late IO.Socket _socket;
-
-
-
-  late final TextEditingController _callIdController = TextEditingController(text: widget.guid);
+  late io.Socket _socket;
 
   RTCPeerConnection? _peerConnection;
   MediaStream? _localStream;
 
-  final _localRenderer = RTCVideoRenderer();
-  final _remoteRenderer = RTCVideoRenderer();
+  final RTCVideoRenderer _localRenderer = RTCVideoRenderer();
+  final RTCVideoRenderer _remoteRenderer = RTCVideoRenderer();
 
   bool _isCalling = false;
 
   bool _isVideoEnabled = true;
   bool _isAudioEnabled = true;
 
-  final Map<String, dynamic> _iceServers = {
-    "iceServers": [
-      {"urls": "stun:stun.l.google.com:19302"},
-      {
-      "urls": "turn:192.158.29.39:3478?transport=udp",
-      "credential": "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      "username": "28224511:1379330808"
-    },
-    {
-      "urls": "turn:192.158.29.39:3478?transport=tcp",
-      "credential": "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
-      "username": "28224511:1379330808"
-    }
+  final Map<String, dynamic> _iceServers = <String, dynamic>{
+    "iceServers": <Map<String, dynamic>>[
+      <String, dynamic>{"urls": "stun:stun.l.google.com:19302"},
+      <String, dynamic>{
+        "urls": "turn:192.158.29.39:3478?transport=udp",
+        "credential": "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+        "username": "28224511:1379330808",
+      },
+      <String, dynamic>{
+        "urls": "turn:192.158.29.39:3478?transport=tcp",
+        "credential": "JZEOEt2V3Qb0y27GRntt2u2PAYA=",
+        "username": "28224511:1379330808",
+      }
     ],
   };
 
@@ -77,6 +65,7 @@ class _CallScreenState extends State<CallScreen> {
   void dispose() {
     _localRenderer.dispose();
     _remoteRenderer.dispose();
+    _localStream?.dispose();
     _socket.dispose();
     super.dispose();
   }
@@ -91,40 +80,42 @@ class _CallScreenState extends State<CallScreen> {
   }
 
   void _connectToSignalingServer() {
-    _socket = IO.io(
-      _signalingServerUrl,
-      IO.OptionBuilder()
-          .setTransports(["websocket"])
+    _socket = io.io(
+      url,
+      io.OptionBuilder()
+          .setTransports(<String>["websocket"])
           .disableAutoConnect()
-          .setExtraHeaders({"Content-Type": "application/json"})
+          .setExtraHeaders(
+            <String, dynamic>{"Content-Type": "application/json"},
+          )
           .build(),
     );
 
     _socket.connect();
 
-    _socket.onConnect((_) {
+    _socket.onConnect((final _) {
       debugPrint("Connected to signaling server");
     });
 
-    _socket.on("offer", (data) async {
+    _socket.on("offer", (final dynamic data) async {
       debugPrint("Received offer: $data");
-      final description = RTCSessionDescription(
+      final RTCSessionDescription description = RTCSessionDescription(
         data["sdp"] as String?,
         data["type"] as String?,
       );
 
-    // _socket.on("teacher-call", (data) {
-    //   final Map<String, String?> data_map = data as Map<String, String>;
-    //   debugPrint('Received "start-call" event from server with data: $data');
-    //   final callId = data_map["callId"] ?? generateCallId(); // In case we decide to generate the id in the server (essential)
-    //   _startCall(callId);
-    //   });
+      // _socket.on("teacher-call", (data) {
+      //   final Map<String, String?> data_map = data as Map<String, String>;
+      //   debugPrint('Received "start-call" event from server with data: $data');
+      //   final callId = data_map["callId"] ?? generateCallId(); // In case we decide to generate the id in the server (essential)
+      //   _startCall(callId);
+      //   });
 
-    // _socket.on("student-call", (data) {
-    //   debugPrint('Received "start-call" event from server with data: $data');
-    //   final String callId = data["callId"] as String ?? generateCallId(); // WE MUST GET THIS FROM THE SERVER!!
-    //   _joinCall(callId);
-    //   });
+      // _socket.on("student-call", (data) {
+      //   debugPrint('Received "start-call" event from server with data: $data');
+      //   final String callId = data["callId"] as String ?? generateCallId(); // WE MUST GET THIS FROM THE SERVER!!
+      //   _joinCall(callId);
+      //   });
 
       if (_peerConnection == null) {
         await _createPeerConnection();
@@ -137,10 +128,11 @@ class _CallScreenState extends State<CallScreen> {
         debugPrint("Error setting remote description (offer): $e");
       }
 
-      final answer = await _peerConnection?.createAnswer();
+      final RTCSessionDescription? answer =
+          await _peerConnection?.createAnswer();
       if (answer != null) {
         await _peerConnection?.setLocalDescription(answer);
-        _socket.emit("answer", {
+        _socket.emit("answer", <String, String?>{
           "callId": widget.guid,
           "sdp": answer.sdp,
           "type": answer.type,
@@ -149,10 +141,10 @@ class _CallScreenState extends State<CallScreen> {
       }
     });
 
-    _socket.on("answer", (data) async {
+    _socket.on("answer", (final dynamic data) async {
       debugPrint("Received answer: $data");
       if (_peerConnection == null) return;
-      final description = RTCSessionDescription(
+      final RTCSessionDescription description = RTCSessionDescription(
         data["sdp"] as String?,
         data["type"] as String?,
       );
@@ -164,11 +156,11 @@ class _CallScreenState extends State<CallScreen> {
       }
     });
 
-    _socket.on("candidate", (data) async {
+    _socket.on("candidate", (final dynamic data) async {
       debugPrint("Received candidate: $data");
       if (_peerConnection == null) return;
 
-      final candidate = RTCIceCandidate(
+      final RTCIceCandidate candidate = RTCIceCandidate(
         data["candidate"]["candidate"] as String?,
         data["candidate"]["sdpMid"] as String?,
         data["candidate"]["sdpMLineIndex"] as int?,
@@ -177,38 +169,41 @@ class _CallScreenState extends State<CallScreen> {
       debugPrint("addCandidate succeeded.");
     });
 
-    _socket.onDisconnect((_) {
+    _socket.onDisconnect((final _) {
       debugPrint("Disconnected from signaling server");
     });
   }
 
-  void _joinCallById(String callId) {
-    _socket.emit("join-call", callId); /// !!!!!
+  void _joinCallById(final String callId) {
+    _socket.emit("join-call", callId);
+
+    /// !!!!!
     debugPrint("emitted join call");
   }
 
   Future<void> _createPeerConnection() async {
     _peerConnection = await createPeerConnection(_iceServers);
 
-    _peerConnection?.onIceConnectionState = (RTCIceConnectionState state) {
+    _peerConnection?.onIceConnectionState =
+        (final RTCIceConnectionState state) {
       debugPrint("ICE state changed: $state");
     };
 
-    _peerConnection?.onConnectionState = (RTCPeerConnectionState state) {
+    _peerConnection?.onConnectionState = (final RTCPeerConnectionState state) {
       debugPrint("Connection state changed: $state");
     };
 
     if (_localStream == null) {
       _localStream = await navigator.mediaDevices
-          .getUserMedia({"video": true, "audio": true});
+          .getUserMedia(<String, dynamic>{"video": true, "audio": true});
       _localRenderer.srcObject = _localStream;
 
-      for (var track in _localStream!.getTracks()) {
-        _peerConnection?.addTrack(track, _localStream!);
+      for (final MediaStreamTrack track in _localStream!.getTracks()) {
+        await _peerConnection?.addTrack(track, _localStream!);
       }
     }
 
-    _peerConnection?.onTrack = (RTCTrackEvent event) {
+    _peerConnection?.onTrack = (final RTCTrackEvent event) {
       debugPrint("Got remote track: ${event.track.id}");
       if (event.streams.isNotEmpty) {
         setState(() {
@@ -217,15 +212,15 @@ class _CallScreenState extends State<CallScreen> {
       }
     };
 
-    _peerConnection?.onIceCandidate = (RTCIceCandidate candidate) {
+    _peerConnection?.onIceCandidate = (final RTCIceCandidate candidate) {
       debugPrint("Sending candidate: ${candidate.toMap()}");
-      _socket.emit("candidate", {
+      _socket.emit("candidate", <String, dynamic>{
         "callId": widget.guid,
         "candidate": candidate.toMap(),
       });
     };
 
-    _peerConnection?.onConnectionState = (state) {
+    _peerConnection?.onConnectionState = (final RTCPeerConnectionState state) {
       debugPrint("Connection state changed: $state");
     };
   }
@@ -240,45 +235,44 @@ class _CallScreenState extends State<CallScreen> {
       debugPrint("PeerConnection created");
     }
     try {
-      final offer = await _peerConnection!.createOffer();
+      final RTCSessionDescription offer = await _peerConnection!.createOffer();
       debugPrint("Created offer: $offer");
 
       await _peerConnection!.setLocalDescription(offer);
       debugPrint("Local description set: $offer");
 
-      _socket.emit("offer", {
+      _socket.emit("offer", <String, String?>{
         "callId": widget.guid,
         "sdp": offer.sdp,
         "type": offer.type,
       });
       debugPrint("Emitted offer to server");
-  } catch (e, s) {
-    debugPrint("Error in createOffer or setLocalDescription: $e\n$s");
-  }
+    } catch (e, s) {
+      debugPrint("Error in createOffer or setLocalDescription: $e\n$s");
+    }
     setState(() {
       _isCalling = true;
     });
   }
 
   Future<void> _joinCall() async {
+    _joinCallById(widget.guid);
 
-  _joinCallById(widget.guid);
+    if (_peerConnection == null) {
+      await _createPeerConnection();
+      debugPrint("PeerConnection created in _joinCall().");
+    }
 
-  if (_peerConnection == null) {
-    await _createPeerConnection();
-    debugPrint("PeerConnection created in _joinCall().");
+    setState(() {
+      _isCalling = true;
+    });
   }
-
-  setState(() {
-    _isCalling = true;
-  });
-}
 
   Future<void> _hangUp() async {
     await _peerConnection?.close();
     _peerConnection = null;
 
-    _localStream?.dispose();
+    await _localStream?.dispose();
     _localStream = null;
 
     setState(() {
@@ -289,66 +283,64 @@ class _CallScreenState extends State<CallScreen> {
   }
 
   void _toggleVideo() {
-  final videoTrack = _localStream?.getVideoTracks().first;
-  if (videoTrack != null) {
-    videoTrack.enabled = !videoTrack.enabled;
-    setState(() {
-      _isVideoEnabled = videoTrack.enabled;
-    });
+    final MediaStreamTrack? videoTrack = _localStream?.getVideoTracks().first;
+    if (videoTrack != null) {
+      videoTrack.enabled = !videoTrack.enabled;
+      setState(() {
+        _isVideoEnabled = videoTrack.enabled;
+      });
+    }
   }
-}
 
-void _toggleAudio() {
-  final audioTrack = _localStream?.getAudioTracks().first;
-  if (audioTrack != null) {
-    audioTrack.enabled = !audioTrack.enabled;
-    setState(() {
-      _isAudioEnabled = audioTrack.enabled;
-    });
+  void _toggleAudio() {
+    final MediaStreamTrack? audioTrack = _localStream?.getAudioTracks().first;
+    if (audioTrack != null) {
+      audioTrack.enabled = !audioTrack.enabled;
+      setState(() {
+        _isAudioEnabled = audioTrack.enabled;
+      });
+    }
   }
-}
 
   @override
   Widget build(final BuildContext context) => Scaffold(
-      appBar: AppBar(
-        title: Text(
-              "Call ID: ${widget.guid}"
+        appBar: AppBar(
+          title: Text("Call ID: ${widget.guid}"),
         ),
-      ),
-      body: Column(
-        children: [
-          Expanded(
-            child: Row(
-              children: [
-                if (_isCalling)
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: Row(
+                children: <Widget>[
+                  if (_isCalling)
+                    Expanded(
+                      child: RTCVideoView(_localRenderer, mirror: true),
+                    ),
                   Expanded(
-                    child: RTCVideoView(_localRenderer, mirror: true),
+                    child: RTCVideoView(_remoteRenderer),
                   ),
-                Expanded(
-                  child: RTCVideoView(_remoteRenderer),
+                ],
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                IconButton(
+                  icon: Icon(
+                    _isVideoEnabled ? Icons.videocam : Icons.videocam_off,
+                  ),
+                  onPressed: _toggleVideo,
+                ),
+                IconButton(
+                  icon: Icon(
+                    _isAudioEnabled ? Icons.mic : Icons.mic_off,
+                  ),
+                  onPressed: _toggleAudio,
                 ),
               ],
             ),
-          ),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              IconButton(
-                icon: Icon(
-                  _isVideoEnabled ? Icons.videocam : Icons.videocam_off,
-                ),
-                onPressed: _toggleVideo,
-              ),
-              IconButton(
-                icon: Icon(
-                  _isAudioEnabled ? Icons.mic : Icons.mic_off,
-                ),
-                onPressed: _toggleAudio,
-              ),
-            ],
-          ),
-          const SizedBox(height: 10),
-        ],
-      ),
-    );
+            const SizedBox(height: 10),
+          ],
+        ),
+      );
 }
