75277afbaa6a1311c5b6c47c9cb1f281932bf62a
yoav kaplan <mechina-hanich-24@greendreamteam.onmicrosoft.com>
2025-01-23 00:56:39 +0200

    added endpoint

--- lib/net/report_techer/report.dart
+++ lib/net/report_techer/report.dart
@@ -2,5 +2,5 @@ import "package:getteacher/net/net.dart";
 import "package:getteacher/net/report_techer/report_teacher_net_model.dart";
 
 Future<void> report (final ReportTeacherNetModel model) async {
-  await getClient().postJson("TODO", model.toJson()); // TODO endpoint
+  await getClient().postJson("Report-teacher-controller", model.toJson());
 }
