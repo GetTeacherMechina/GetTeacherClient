d8ca6a264885df99be80b143d23ef7a1bb055cb0
yoav kaplan <mechina-hanich-24@greendreamteam.onmicrosoft.com>
2025-01-23 02:01:29 +0200

    refactor

--- lib/net/meeting_history/meetings_history.dart
+++ lib/net/meeting_history/meetings_history.dart
@@ -2,7 +2,8 @@ import "package:getteacher/net/meeting_history/meeting_history_net_model.dart";
 import "package:getteacher/net/net.dart";
 
 Future<MeetingsHistoryNetModelRequst> getMeetingsHistory(
-        final MeetingsHistoryNetModelRespons response,) async =>
+  final MeetingsHistoryNetModelRespons response,
+) async =>
     MeetingsHistoryNetModelRequst.fromJson(
       await getClient().postJson("/MeetingsHistory", response.toJson()),
     );
