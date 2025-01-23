0e5f2e8ca9a97c212f84b0ad696f5395e0ef1592
aryeh15 <mechina-hanich-15@greendreamteam.onmicrosoft.com>
2025-01-23 09:59:20 +0200

    made create chat better look

--- lib/views/chats_main_screen/create_chat_screen/create_chat_screen.dart
+++ lib/views/chats_main_screen/create_chat_screen/create_chat_screen.dart
@@ -2,6 +2,7 @@ import "package:flutter/material.dart";
 import "package:getteacher/common_widgets/searcher_widget.dart";
 import "package:getteacher/net/chats/chats.dart";
 import "package:getteacher/net/users/users.dart";
+import "package:getteacher/theme/theme.dart";
 
 class CreateChatScreen extends StatefulWidget {
   const CreateChatScreen({super.key});
@@ -39,30 +40,55 @@ class _CreateChatScreenState extends State<CreateChatScreen> {
   Widget build(final BuildContext context) => Scaffold(
         appBar: AppBar(
           title: const Text("Create Chat - Select Teachers"),
+          backgroundColor: AppTheme.primaryColor,
+          foregroundColor: AppTheme.whiteColor,
         ),
-        body: SearcherWidget<(UserDetails, bool)>(
-          getItemString: (final (UserDetails, bool) p0) => p0.$1.user.userName,
-          fetchItems: () async => users,
-          itemBuilder: (
-            final BuildContext context,
-            final (UserDetails, bool) userDetails,
-          ) =>
-              ListTile(
-            title: Text(userDetails.$1.user.userName),
-            leading: Checkbox(
-              value: userDetails.$2,
-              onChanged: (final _) {
-                setState(() {
-                  users[users.indexOf(userDetails)] =
-                      (userDetails.$1, !userDetails.$2);
-                });
-              },
+        body: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: SearcherWidget<(UserDetails, bool)>(
+            getItemString: (final (UserDetails, bool) p0) =>
+                p0.$1.user.userName,
+            fetchItems: () async => users,
+            itemBuilder: (
+              final BuildContext context,
+              final (UserDetails, bool) userDetails,
+            ) =>
+                ListTile(
+              contentPadding:
+                  const EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
+              title: Text(
+                userDetails.$1.user.userName,
+                style: AppTheme.bodyTextStyle,
+              ),
+              subtitle: Text(
+                userDetails.$1.teacher != null
+                    ? (userDetails.$1.teacher!.bio)
+                    : "",
+                style: AppTheme.bodyTextStyle,
+              ),
+              trailing: Checkbox(
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5),
+                ),
+                value: userDetails.$2,
+                activeColor: AppTheme.accentColor,
+                onChanged: (final _) {
+                  setState(() {
+                    users[users.indexOf(userDetails)] =
+                        (userDetails.$1, !userDetails.$2);
+                  });
+                },
+              ),
             ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
           onPressed: onCreateChat,
-          child: const Icon(Icons.create),
+          backgroundColor: AppTheme.accentColor,
+          child: const Icon(
+            Icons.chat,
+            size: 28,
+          ),
         ),
       );
 }
